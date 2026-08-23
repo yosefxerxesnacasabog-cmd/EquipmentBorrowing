58581c9b282a196107aa7d22cdea8f1e22cacb03
Yosef Xerxes Nacasabog <yosefxerxesnacasabog@gmail.com>
2026-08-23 15:19:19 +0800

    Add in-memory student repository

---
+++ src/EquipmentBorrowing.Infrastructure/Repositories/InMemoryStudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Infrastructure.Repositories;
+
+public class InMemoryStudentRepository : IStudentRepository
+{
+    private readonly List<Student> _students = new()
+    {
+        new Student(1, "Juan Dela Cruz", true),
+        new Student(2, "Maria Santos", true),
+        new Student(3, "Pedro Reyes", false)
+    };
+
+    public Task<Student?> GetByIdAsync(
+        int id,
+        CancellationToken cancellationToken = default)
+    {
+        var student = _students.FirstOrDefault(s => s.Id == id);
+
+        return Task.FromResult(student);
+    }
+}
