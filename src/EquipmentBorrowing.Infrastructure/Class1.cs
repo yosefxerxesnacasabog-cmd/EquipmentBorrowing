6ca3f5d7618d9f4114df1886928b1b50492decb9
Yosef Xerxes Nacasabog <yosefxerxesnacasabog@gmail.com>
2026-08-23 15:04:00 +0800

    Add in-memory equipment repository

--- src/EquipmentBorrowing.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EquipmentBorrowing.Infrastructure;
-
-public class Class1
-{
-
-}
---
+++ src/EquipmentBorrowing.Infrastructure/Repositories/InMemoryEquipmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Infrastructure.Repositories;
+
+public class InMemoryEquipmentRepository : IEquipmentRepository
+{
+    private readonly List<Equipment> _equipment = new();
+
+    public Task<Equipment?> GetByIdAsync(
+        int id,
+        CancellationToken cancellationToken = default)
+    {
+        var equipment = _equipment.FirstOrDefault(e => e.Id == id);
+
+        return Task.FromResult(equipment);
+    }
+}
